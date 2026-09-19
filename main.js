7ab149ec4585523bc1475718acfc1daeec7112de
Milan Zolota <milan.zolota@gmail.com>
2026-09-19 18:00:52 +0200

    nevegetariansky hamburger

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 $('#pastShows').on('shown.bs.collapse', function () {
     this.scrollIntoView();
 });
+
+$('#navbarSupportedContent .nav-link').on('click', function () {
+    if ($('.navbar-toggler').is(':visible')) {
+        bootstrap.Collapse.getOrCreateInstance(document.getElementById('navbarSupportedContent'), { toggle: false }).hide();
+    }
+});
